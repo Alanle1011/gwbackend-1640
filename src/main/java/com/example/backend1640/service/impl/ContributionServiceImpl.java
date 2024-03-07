3dfc82e60061ba0a9aa02771b4bfa7bd397f1a2a
Alanle AXO <lephuc@axo.business>
2024-03-07 18:16:16 +0700

    set uploader when create coordinator

--- src/main/java/com/example/backend1640/service/impl/ContributionServiceImpl.java
+++ src/main/java/com/example/backend1640/service/impl/ContributionServiceImpl.java
@@ -5,34 +5,39 @@ import com.example.backend1640.dto.ContributionDTO;
 import com.example.backend1640.dto.CreateContributionDTO;
 import com.example.backend1640.entity.Contribution;
 import com.example.backend1640.entity.User;
-import com.example.backend1640.exception.ContributionAlreadyExistsException;
+import com.example.backend1640.exception.UserNotExistsException;
 import com.example.backend1640.repository.ContributeRepository;
+import com.example.backend1640.repository.UserRepository;
 import com.example.backend1640.service.ContributionService;
-import com.fasterxml.jackson.databind.util.BeanUtil;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
-import java.util.Objects;
 import java.util.Optional;
 
 @Service
 public class ContributionServiceImpl implements ContributionService {
     private final ContributeRepository contributionRepository;
+    private final UserRepository userRepository;
 
-    public ContributionServiceImpl(ContributeRepository contributionRepository) {
+    public ContributionServiceImpl(ContributeRepository contributionRepository, UserRepository userRepository) {
         this.contributionRepository = contributionRepository;
+        this.userRepository = userRepository;
     }
 
     @Override
     public ContributionDTO createContribution(CreateContributionDTO contributionDTO) {
+        User uploader = validateUserNotExists(contributionDTO.getUploaded_user_id());
+//        User coordinator = validateUserNotExists(uploader.getFaculty());
+
         Contribution contribution = new Contribution();
         BeanUtils.copyProperties(contributionDTO, contribution);
         contribution.setCreated_at(new Date());
         contribution.setUpdated_at(new Date());
         contribution.setStatus(StatusEnum.OPEN);
+        contribution.setUploaded_user_id(uploader);
+
         contribution.setApproved_coordinator_id(null);//TESTTTT
-        contribution.setUploaded_user_id(null);//TESTTT
 
         //Save
         Contribution savedContribution = contributionRepository.save(contribution);
@@ -42,6 +47,15 @@ public class ContributionServiceImpl implements ContributionService {
         BeanUtils.copyProperties(savedContribution, returnedContribution);
 
         return returnedContribution;
+    }
+
+    private User validateUserNotExists(Long id) {
+        Optional<User> optionalUser = userRepository.findById(id);
+
+        if (optionalUser.isEmpty()) {
+            throw new UserNotExistsException("User Not Exists");
+        }
+        return optionalUser.get();
     }
 
 }
