3bfe83b61f5d896ff9818c2f0e7aa9ad3262360f
LAPTOP-ALANLE\AlanLe <alanhoangphuc1011@gmail.com>
2024-04-02 21:13:30 +0700

    implement CORS

--- src/main/java/com/example/backend1640/controller/DocumentController.java
+++ src/main/java/com/example/backend1640/controller/DocumentController.java
@@ -9,7 +9,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.ByteArrayOutputStream;
@@ -32,14 +39,14 @@ public class DocumentController {
     }
 
     @GetMapping
-    public String getAllDocument(Model model){
+    public String getAllDocument(Model model) {
         List<Document> documents = documentService.getAllDocuments();
         model.addAttribute("documents", documents);
         return "document";
     }
 
     @PostMapping
-    public String saveDocument(@RequestParam("doc") MultipartFile[] files, @RequestParam(value="contributionId", required=true) String contributionId) throws IOException {
+    public String saveDocument(@RequestParam("doc") MultipartFile[] files, @RequestParam(value = "contributionId", required = true) String contributionId) throws IOException {
         for (MultipartFile file : files) {
             documentService.saveDocument(file, contributionId);
         }
@@ -48,14 +55,14 @@ public class DocumentController {
     }
 
     @PutMapping
-    public void updateDocument(@RequestParam("doc") MultipartFile[] files, @RequestParam(value="documentId", required=true) String documentId) throws IOException {
+    public void updateDocument(@RequestParam("doc") MultipartFile[] files, @RequestParam(value = "documentId", required = true) String documentId) throws IOException {
         for (MultipartFile file : files) {
             documentService.updateDocument(file, documentId);
         }
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ByteArrayResource> downloadDocument(@PathVariable long id){
+    public ResponseEntity<ByteArrayResource> downloadDocument(@PathVariable long id) {
         Document document = documentService.getDocument(id).get();
         return ResponseEntity.ok()
                 .contentType(MediaType.parseMediaType(document.getType()))
