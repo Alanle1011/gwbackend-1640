0a7951544f92c8cb54f37279fa866ce8bfa39b7a
peguin818 <pcc1327@gmail.com>
2024-04-19 02:28:00 +0700

    Tweaked application

--- src/main/java/com/example/backend1640/entity/Comment.java
+++ src/main/java/com/example/backend1640/entity/Comment.java
@@ -19,7 +19,7 @@ public class Comment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(cascade = CascadeType.ALL)
+    @ManyToOne
     @JoinColumn(name = "USER_ID", referencedColumnName = "id")
     private User user;
 
